98e12db46d1885c1b631112bcce79b7f92a750e5
Xabiergarai <72606708+Xabiergarai@users.noreply.github.com>
2022-10-20 14:14:39 +0200

    Clase Carrito y Admin completados

--- jxj-computers/src/jxj/clasesBasicas/Administrador.java
+++ jxj-computers/src/jxj/clasesBasicas/Administrador.java
@@ -1,10 +1,61 @@
 package jxj.clasesBasicas;
 
-public class Administrador {
+public class Administrador extends Usuario {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String AdminUsuario, AdminInterfaz, AdminVerificar, AdminRecuperacion;
 
+	public Administrador(int id, String nombre, String apellidos, String usuario, String contrasenia, String email,
+			String adminUsuario, String adminInterfaz, String adminVerificar, String adminRecuperacion) {
+		super(id, nombre, apellidos, usuario, contrasenia, email);
+		AdminUsuario = adminUsuario;
+		AdminInterfaz = adminInterfaz;
+		AdminVerificar = adminVerificar;
+		AdminRecuperacion = adminRecuperacion;
+	}
+	
+	public Administrador() {
+		super();
+		this.AdminUsuario = " ";
+		this.AdminInterfaz = " ";
+		this.AdminVerificar = " ";
+		this.AdminRecuperacion = " ";
 	}
 
+	public String getAdminUsuario() {
+		return AdminUsuario;
+	}
+
+	public void setAdminUsuario(String adminUsuario) {
+		AdminUsuario = adminUsuario;
+	}
+
+	public String getAdminInterfaz() {
+		return AdminInterfaz;
+	}
+
+	public void setAdminInterfaz(String adminInterfaz) {
+		AdminInterfaz = adminInterfaz;
+	}
+
+	public String getAdminVerificar() {
+		return AdminVerificar;
+	}
+
+	public void setAdminVerificar(String adminVerificar) {
+		AdminVerificar = adminVerificar;
+	}
+
+	public String getAdminRecuperacion() {
+		return AdminRecuperacion;
+	}
+
+	public void setAdminRecuperacion(String adminRecuperacion) {
+		AdminRecuperacion = adminRecuperacion;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrador [AdminUsuario=" + AdminUsuario + ", AdminInterfaz=" + AdminInterfaz + ", AdminVerificar="
+				+ AdminVerificar + ", AdminRecuperacion=" + AdminRecuperacion + "]";
+	}
 }
