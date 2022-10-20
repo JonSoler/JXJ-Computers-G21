48e22ba2229f2509db194ba7ca8be43d7686876d
Xabiergarai <72606708+Xabiergarai@users.noreply.github.com>
2022-10-20 14:06:13 +0200

    Listas Usuario y dispositivo creadas

--- jxj-computers/src/jxj/clasesBasicas/ListaUsuario.java
+++ jxj-computers/src/jxj/clasesBasicas/ListaUsuario.java
@@ -1,10 +1,22 @@
 package jxj.clasesBasicas;
 
+import java.util.ArrayList;
+
+
 public class ListaUsuario {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private ArrayList<Usuario> usuarios;
 
+	public ListaUsuario() {
+		this.usuarios = new ArrayList<Usuario>();
 	}
+
+	public void setUsuarios(ArrayList<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public ArrayList<Usuario> getUsuarios() {
+		return usuarios;
+	}	
 
 }
