7c4aced519c0debefbb7efb2b6879b064c488932
Xabiergarai <72606708+Xabiergarai@users.noreply.github.com>
2022-10-21 13:08:53 +0200

    Admin test y carrito test completados y funcionando

---
+++ jxj-computers/test/tests/AdministradorTest.java
@@ -0,0 +1,70 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jxj.clasesBasicas.Administrador;
+
+class AdministradorTest {
+
+	private Administrador admin;
+	private Administrador admin1;
+
+	
+	@BeforeEach
+	public void setUp() {
+		 admin = new Administrador(01,"x.g", "02", "x@gmail.com", "xabier", "garai", "12", "1", "2", "2");
+		 admin1 = new Administrador();
+	}
+
+	@Test
+	public void testSetAdminUsuario() {
+		admin.setAdminUsuario("100");
+		assertEquals("100", admin.getAdminUsuario());
+	}
+	@Test
+	public void testGetAdminUsuario() {
+		assertEquals("12", admin.getAdminUsuario());
+	}
+	
+	@Test
+	public void testSetAdminInterfaz() {
+		admin.setAdminInterfaz("23");
+		assertEquals("23", admin.getAdminInterfaz());
+	}
+	@Test
+	public void testGetAdminInterfaz() {
+		assertEquals("1", admin.getAdminInterfaz());
+	}
+	
+	@Test
+	public void testSetAdminVerificar() {
+		admin.setAdminVerificar("30");
+		assertEquals("30", admin.getAdminVerificar());
+	}
+	
+	@Test
+	public void testGetAdminVerificar() {
+		assertEquals("2", admin.getAdminVerificar());
+	}
+	
+	@Test
+	public void testSetAdminRecuperacion() {
+		admin.setAdminRecuperacion("5");
+		assertEquals("5", admin.getAdminRecuperacion());
+	}
+	
+	@Test
+	public void testGetAdminRecuperacion() {
+		assertEquals("2", admin.getAdminRecuperacion());
+	}
+	
+	@Test
+	public void testToString() {
+		assertEquals("Administrador [AdminUsuario=12, AdminInterfaz=1, AdminVerificar=2, AdminRecuperacion=2]"
+		, admin.toString());
+	}
+}
+
