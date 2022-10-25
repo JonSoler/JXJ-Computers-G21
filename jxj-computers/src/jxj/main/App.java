87f4ce25a675943dc8e7cc86ff484f364b770dea
Xabiergarai <72606708+Xabiergarai@users.noreply.github.com>
2022-10-25 15:54:33 +0200

    Ventana main actualizada para lanzar la aplicación

--- jxj-computers/src/jxj/main/App.java
+++ jxj-computers/src/jxj/main/App.java
@@ -1,5 +1,35 @@
 package jxj.main;
 
+import java.awt.EventQueue;
+import java.util.logging.Logger;
+
+import jxj.main.App;
+import jxj.ventanasPrimarias.VentanaInicio;
+
 public class App {
+	
+	public static Logger logger = Logger.getLogger(App.class.getName());
+
+	public static void main(String[] args) {
+		
+		logger.info("Programa comenzado");
+		
+	
+
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					VentanaInicio vi = new VentanaInicio();
+					vi.setVisible(true);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+		
+	}
 
 }
