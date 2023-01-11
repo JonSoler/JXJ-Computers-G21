cc304df5a6e2bbe6a1c1fdc53da94f896f7173fd
JonSoler <jon.soler@opendeusto.es>
2023-01-11 16:49:23 +0100

    Método recursivo que recoge móviles según precio

--- jxj-computers/src/jxj/recursividad/RecursividadRecomendaciones.java
+++ jxj-computers/src/jxj/recursividad/RecursividadRecomendaciones.java
@@ -8,5 +8,22 @@ public class RecursividadRecomendaciones {
 	
 	public RecursividadRecomendaciones(double importe, ArrayList<Movil> ad) {
 		al = new ArrayList<>();
+		r(importe, al, new ArrayList<Movil>(), ad, 0);
+	}
+	
+	public void r (double importe, ArrayList<ArrayList<Movil>> al, ArrayList<Movil> aux, ArrayList<Movil> ad, int i) {
+		if(i<ad.size()) {
+			if(importe>0) {
+				importe = importe - ad.get(i).getPrecio();
+				aux.add(ad.get(i));
+				r(importe, al, aux, ad, i+1);
+				//importe = importe + ad.get(i).getPrecio();
+				//aux.remove(i);
+			}else {
+				al.add(aux);
+			}
+		}else {
+			al.add(aux);
+		}
 	}
 }
