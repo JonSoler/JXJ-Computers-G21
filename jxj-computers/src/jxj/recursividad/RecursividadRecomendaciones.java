dfdf911e0ec444be3f6006f1750b4532a2e674f0
JonSoler <jon.soler@opendeusto.es>
2023-01-11 14:38:11 +0100

    Crear RecursividadRecomendaciones

---
+++ jxj-computers/src/jxj/recursividad/RecursividadRecomendaciones.java
@@ -0,0 +1,12 @@
+package jxj.recursividad;
+
+import java.util.ArrayList;
+import jxj.seccionDisp.Movil;
+
+public class RecursividadRecomendaciones {
+	private ArrayList<ArrayList<Movil>> al;
+	
+	public RecursividadRecomendaciones(double importe, ArrayList<Movil> ad) {
+		al = new ArrayList<>();
+	}
+}
